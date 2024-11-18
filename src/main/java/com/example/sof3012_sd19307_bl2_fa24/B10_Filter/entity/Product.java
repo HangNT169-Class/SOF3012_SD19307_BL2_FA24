f8a88b067c15a72e5efe48ea228f8a68e6d77f6f
HangNT169 <HangNT169@fpt.edu.vn>
2024-11-18 11:16:57 +0700

    Buổi 10: Filter - Authen - Phần 1 - Xác thực tài khoản có được truy cập vào hệ thống không

---
+++ src/main/java/com/example/sof3012_sd19307_bl2_fa24/B10_Filter/entity/Product.java
@@ -0,0 +1,43 @@
+package com.example.sof3012_sd19307_bl2_fa24.B10_Filter.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "product_code", length = 100)
+    private String productCode;
+
+    @Nationalized
+    @Column(name = "product_name", length = 100)
+    private String productName;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "description", length = 100)
+    private String description;
+
+}
